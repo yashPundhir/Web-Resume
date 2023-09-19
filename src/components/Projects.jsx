0c0a1f346f91e9195c952b09f091d38012467646
yashPundhir <yash.pundhir.prof@gmail.com>
2023-09-19 23:30:54 +0530

    Projects component done

---
+++ src/components/Projects.jsx
@@ -0,0 +1,187 @@
+const Projects = () => {
+	return (
+		<div className="flex flex-col gap-2.5">
+			<div className="w-full">
+				<h1 className="text-[40px] font-semibold ">PROJECTS</h1>
+				<div className="border-2 border-black mr-4 -mt-1"></div>
+			</div>
+			<div>
+				<h1 className="text-2xl font-medium">Clips-Cave</h1>
+				<h1 className="text-[15px] mt-1 mr-5">
+					A YouTube like Video Streaming Application where the user can Play the
+					Video, Search for Videos, Filter Out Categories, Explore Channel
+					Detail page, etc.
+				</h1>
+				<ul className="list-disc ml-[22px] mt-2 mr-3 text-[15px]">
+					<li>Used React to create the UI of the App.</li>
+					<li>Used TailwindCSS to do the styling of the App.</li>
+					<li>Used RapidAPI to fetch the required data in JSON format.</li>
+				</ul>
+				<div className="mt-1 flex gap-10">
+					<div className="flex gap-1 items-center justify-start">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							fill="currentColor"
+							className="bi bi-link-45deg mt-1"
+							viewBox="0 0 16 16"
+						>
+							<path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z" />
+							<path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z" />
+						</svg>
+						<span className="text-[15px] font-semibold underline">
+							<a
+								href="https://clips-cave-sj.netlify.app/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Live Link
+							</a>
+						</span>
+					</div>
+					<div className="flex gap-2 items-center justify-start">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="15"
+							height="15"
+							fill="currentColor"
+							className="bi bi-github mt-0.5"
+							viewBox="0 0 16 16"
+						>
+							<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+						</svg>
+						<span className="text-[15px] font-semibold underline">
+							<a
+								href="https://github.com/yashPundhir/Clips-Cave"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Source Code Link
+							</a>
+						</span>
+					</div>
+				</div>
+			</div>
+			<div className="mt-2 border-2 border-dashed border-gray-300 mr-4"></div>
+			<div>
+				<h1 className="text-2xl font-medium">Ride-Spectrum</h1>
+				<h1 className="text-[15px] mt-1 mr-5">
+					A Car Showcase Platform where users can search for any car model and
+					get the corresponding details of that car.
+				</h1>
+				<ul className="list-disc ml-[22px] mt-2 mr-3 text-[15px]">
+					<li>Used React with NextJS to create the UI of the App.</li>
+					<li>Used TailwindCSS to do the styling of the App.</li>
+					<li>Used RapidAPI to fetch the required data in JSON format.</li>
+				</ul>
+				<div className="mt-1 flex gap-10">
+					<div className="flex gap-1 items-center justify-start">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							fill="currentColor"
+							className="bi bi-link-45deg mt-1"
+							viewBox="0 0 16 16"
+						>
+							<path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z" />
+							<path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z" />
+						</svg>
+						<span className="text-[15px] font-semibold underline">
+							<a
+								href="https://ride-spectrum-sj.vercel.app/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Live Link
+							</a>
+						</span>
+					</div>
+					<div className="flex gap-2 items-center justify-start">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="15"
+							height="15"
+							fill="currentColor"
+							className="bi bi-github mt-0.5"
+							viewBox="0 0 16 16"
+						>
+							<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+						</svg>
+						<span className="text-[15px] font-semibold underline">
+							<a
+								href="https://github.com/yashPundhir/Ride-Spectrum"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Source Code Link
+							</a>
+						</span>
+					</div>
+				</div>
+			</div>
+			<div className="mt-2 border-2 border-dashed border-gray-300 mr-4"></div>
+			<div>
+				<h1 className="text-2xl font-medium">Fit-Blitz</h1>
+				<h1 className="text-[15px] mt-1 mr-5">
+					A Fitness & Gym Exercises Application where the user can Search for
+					the Exercise Description related to a Target Muscle, Body Part,
+					Equipments, etc.
+				</h1>
+				<ul className="list-disc ml-[22px] mt-2 mr-3 text-[15px]">
+					<li>Used React to create the UI of the App.</li>
+					<li>Used TailwindCSS to do the styling of the App.</li>
+					<li>Used RapidAPI to fetch the required data in JSON format.</li>
+				</ul>
+				<div className="mt-1 flex gap-10">
+					<div className="flex gap-1 items-center justify-start">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							fill="currentColor"
+							className="bi bi-link-45deg mt-1"
+							viewBox="0 0 16 16"
+						>
+							<path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z" />
+							<path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z" />
+						</svg>
+						<span className="text-[15px] font-semibold underline">
+							<a
+								href="https://fit-blitz-sj.netlify.app/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Live Link
+							</a>
+						</span>
+					</div>
+					<div className="flex gap-2 items-center justify-start">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="15"
+							height="15"
+							fill="currentColor"
+							className="bi bi-github mt-0.5"
+							viewBox="0 0 16 16"
+						>
+							<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+						</svg>
+						<span className="text-[15px] font-semibold underline">
+							<a
+								href="https://github.com/yashPundhir/FitBlitz"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Source Code Link
+							</a>
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Projects;
