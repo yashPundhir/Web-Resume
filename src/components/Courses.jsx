301ade2384644c330beb362fa794cb4eb0c43d51
yashPundhir <yash.pundhir.prof@gmail.com>
2023-09-19 23:25:48 +0530

    Courses component done

---
+++ src/components/Courses.jsx
@@ -0,0 +1,17 @@
+const Courses = () => {
+	return (
+		<div className="flex flex-col gap-2.5">
+			<div className="w-full">
+				<h1 className="text-[40px] font-semibold ">COURSES</h1>
+				<div className="border-2 border-black  -mt-1"></div>
+			</div>
+			<div>
+				<h1 className="text-xl font-bold">
+					Full Stack JavaScript Web Developer Bootcamp - iNeuron
+				</h1>
+			</div>
+		</div>
+	);
+};
+
+export default Courses;
