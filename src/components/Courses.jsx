751b4b3b3e87849dd180e7044b5e3883c99d14b5
yashPundhir <yash.pundhir.prof@gmail.com>
2023-10-03 23:37:58 +0530

    UI updated

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -2,11 +2,11 @@ const Courses = () => {
 	return (
 		<div className="flex flex-col gap-2.5">
 			<div className="w-full">
-				<h1 className="text-[40px] font-semibold ">COURSES</h1>
+				<h1 className="text-[31px] font-semibold ">COURSES</h1>
 				<div className="border-2 border-black  -mt-1"></div>
 			</div>
 			<div>
-				<h1 className="text-xl font-bold">
+				<h1 className="text-xl font-semibold">
 					Full Stack JavaScript Web Developer Bootcamp - iNeuron
 				</h1>
 			</div>
